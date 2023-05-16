9ce02f7537310928a2d2d66350ea24797aecfa32
Ahmed-Mohammed-11 <ahmedmo1162002@gmail.com>
2023-05-16 11:26:12 +0300

    Project Construction

--- src/main/java/com/example/BackendPerfectHashing/IPerfectHash.java
+++ src/main/java/com/example/BackendPerfectHashing/IPerfectHash.java
@@ -1,4 +1,7 @@
 package com.example.BackendPerfectHashing;
 
 public interface IPerfectHash {
+    boolean insert(String item);
+    boolean delete(String item);
+    boolean search(String item);
 }
