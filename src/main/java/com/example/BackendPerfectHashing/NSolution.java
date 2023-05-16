02781911d3f762373e5b85a3a7d5a60749da2ff2
Ahmed-Mohammed-11 <ahmedmo1162002@gmail.com>
2023-05-16 11:45:06 +0300

    Project Construction

--- src/main/java/com/example/BackendPerfectHashing/NSolution.java
+++ src/main/java/com/example/BackendPerfectHashing/NSolution.java
@@ -1,4 +1,13 @@
 package com.example.BackendPerfectHashing;
 
-public class NSolution {
+public class NSolution implements IPerfectHash{
+    public boolean insert(String item){
+        return false;
+    }
+    public boolean delete(String item){
+        return false;
+    }
+    public boolean search(String item){
+        return false;
+    }
 }
--- src/main/java/com/example/BackendPerfectHashing/NSquaredSolution.java
+++ src/main/java/com/example/BackendPerfectHashing/NSquaredSolution.java
@@ -1,4 +1,14 @@
 package com.example.BackendPerfectHashing;
 
-public class NSquaredSolution {
+public class NSquaredSolution implements IPerfectHash{
+    public boolean insert(String item){
+        return false;
+    }
+    public boolean delete(String item){
+        return false;
+    }
+    public boolean search(String item){
+        return false;
+    }
+
 }
