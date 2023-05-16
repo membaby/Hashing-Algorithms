b3e106e9d770377fa997ec3659d6ea5f8f3e9966
Ahmed-Mohammed-11 <ahmedmo1162002@gmail.com>
2023-05-16 11:08:27 +0300

    Project Construction

---
+++ src/main/java/com/example/BackendPerfectHashing/NSquaredSolution.java
@@ -0,0 +1,4 @@
+package com.example.BackendPerfectHashing;
+
+public class NSquaredSolution {
+}
--- src/main/java/com/example/PerfectHashingApp.java
+++ src/main/java/com/example/PerfectHashingApp.java
@@ -4,10 +4,10 @@ package com.example;
  * Hello world!
  *
  */
-public class App 
+public class PerfectHashingApp
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello World! 123" );
+        //this main method is to take user input and talk to CLI class
     }
 }
