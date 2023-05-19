966a407adff66124ffac3409ccf00f6dece9b8ce
Ahmed-Mohammed-11 <ahmedmo1162002@gmail.com>
2023-05-19 16:13:41 +0300

    UniverseHashing

--- src/main/java/com/example/BackendPerfectHashing/UniverseHashing.java
+++ src/main/java/com/example/BackendPerfectHashing/UniverseHashing.java
@@ -1,7 +1,19 @@
 package com.example.BackendPerfectHashing;
 
+import java.util.Arrays;
+
 public class UniverseHashing
 {
+    private int u = 64 ;
+    private String hashString = "";
+
+    public void setHashString(String hashString) {
+        this.hashString = hashString;
+    }
+
+    public String getHashString() {
+        return hashString;
+    }
 
     public String hash_string(String str)
     {
@@ -13,5 +25,48 @@ public class UniverseHashing
         return Long.toBinaryString(code);
     }
 
+    public String hash(String binaryString , int tableSize)
+    {
+
+        //number of rows | index size
+        int b = (int) Math.ceil(Math.log(tableSize) / Math.log(2));
+        int[][] hashMatrix = new int[b][u];
+
+        //construct the hash matrix
+        for(int i = 0 ; i < b ; i ++)
+        {
+            for(int j = 0 ; j < u ; j++)
+            {
+                hashMatrix[i][j] = (int) (Math.random() * 2);
+            }
+        }
+
+        int[] hx = new int[b];
+        //construct the hx | array of hash digest
+        for(int i = 0 ; i < binaryString.length() ; i ++)
+        {
+            if(binaryString.charAt(i) == '1')
+            {
+                for(int j = 0 ; j < b ; j++)
+                {
+                    hx[j] = (hx[j] + hashMatrix[j][i]) % 2;
+                }
+            }
+        }
+
+        //return the hash digest to caller
+        String digest = "";
+        for(int i = 0 ; i < b ; i ++)
+        {
+            digest += hx[i];
+        }
+
+        return digest;
+    }
 }
+
+
+
+
+
 
--- src/main/java/com/example/PerfectHashingApp.java
+++ src/main/java/com/example/PerfectHashingApp.java
@@ -1,5 +1,9 @@
 package com.example;
 
+import com.example.BackendPerfectHashing.UniverseHashing;
+
+import java.util.Arrays;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,15 @@ public class PerfectHashingApp
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        UniverseHashing universeHashing = new UniverseHashing();
+        String binaryString = universeHashing.hash_string("hello");
+        System.out.println(binaryString);
+        String hx = universeHashing.hash(binaryString, 100);
+        universeHashing.setHashString(hx);
+        for(int i = 0 ; i < 100 ; i ++)
+        {
+            //test that getters and setters works just fine
+            System.out.println(universeHashing.getHashString());
+        }
     }
 }
