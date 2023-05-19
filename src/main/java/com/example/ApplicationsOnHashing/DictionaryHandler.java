25a80892be8d26a65818edd046c58731f0c3ec98
Ahmed-Mohammed-11 <ahmedmo1162002@gmail.com>
2023-05-19 17:02:56 +0300

    UniverseHashing and CLI

---
+++ src/main/java/com/example/ApplicationsOnHashing/DictionaryHandler.java
@@ -0,0 +1,59 @@
+package com.example.ApplicationsOnHashing;
+
+import java.util.Scanner;
+
+public class DictionaryHandler
+{
+    private EnglishDictionary englishDictionary;
+    public DictionaryHandler(String methodChoice, int tableSize, String hashTable)
+    {
+        englishDictionary = new EnglishDictionary(hashTable, tableSize);
+        Scanner scanner = new Scanner(System.in);
+        switch (methodChoice)
+        {
+            case "1":
+                System.out.println("Enter the word to insert: ");
+                String word = scanner.next();
+                if (englishDictionary.insert(word)) {
+                    System.out.println("Word inserted successfully");
+                } else {
+                    System.out.println("Word already exists");
+                }
+                break;
+            case "2":
+                System.out.println("Enter the word to delete: ");
+                word = scanner.next();
+                if (englishDictionary.delete(word)) {
+                    System.out.println("Word deleted successfully");
+                } else {
+                    System.out.println("Word doesn't exist");
+                }
+                break;
+
+            case "3":
+                System.out.println("Enter the word to search for: ");
+                word = scanner.next();
+                if (englishDictionary.search(word)) {
+                    System.out.println("Word found");
+                } else {
+                    System.out.println("Word not found");
+                }
+                break;
+            case "4":
+                System.out.println("Enter the path of the file to insert: ");
+                String filePath = scanner.next();
+                int count = englishDictionary.batchInsert(filePath);
+                System.out.println(count + " words inserted successfully");
+                break;
+            case "5":
+                System.out.println("Enter the path of the file to delete: ");
+                filePath = scanner.next();
+                count = englishDictionary.batchDelete(filePath);
+                System.out.println(count + " words deleted successfully");
+                break;
+            default:
+                System.out.println("Invalid choice");
+                break;
+        }
+    }
+}
--- src/main/java/com/example/PerfectHashingApp.java
+++ src/main/java/com/example/PerfectHashingApp.java
@@ -1,8 +1,13 @@
 package com.example;
 
+import com.example.ApplicationsOnHashing.DictionaryHandler;
+import com.example.ApplicationsOnHashing.EnglishDictionary;
 import com.example.BackendPerfectHashing.UniverseHashing;
 
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.Scanner;
+
 
 /**
  * Hello world!
@@ -10,8 +15,26 @@ import java.util.Arrays;
  */
 public class PerfectHashingApp
 {
-    public static void main( String[] args )
-    {
+
+    public static void printTableMenu(){
+        System.out.println("Choices");
+        System.out.println("1. O(N2) solution");
+        System.out.println("2. O(N) solution");
+        System.out.println("3. Exit");
+        System.out.println("Choose your hash method: ");
+    }
+    public static void printActionMenu(){
+        System.out.println("Choices");
+        System.out.println("1. Insert a word");
+        System.out.println("2. Delete a word");
+        System.out.println("3. Search for a word");
+        System.out.println("4. Batch insert");
+        System.out.println("5. Batch delete");
+        System.out.println("6. Exit");
+        System.out.println("Enter an action to perform: ");
+    }
+
+    public static void main( String[] args ) throws IOException, InterruptedException {
         UniverseHashing universeHashing = new UniverseHashing();
         String binaryString = universeHashing.hash_string("hello");
         System.out.println(binaryString);
@@ -21,6 +44,25 @@ public class PerfectHashingApp
         {
             //test that getters and setters works just fine
             System.out.println(universeHashing.getHashString());
+        }
+
+        //CLI implementation
+        Scanner scanner = new Scanner(System.in);
+        EnglishDictionary englishDictionary ;
+        String tableChoice = "";
+        String methodChoice = "";
+        int tableSize = 0;
+        printTableMenu();
+        tableChoice = scanner.next();
+        if(tableChoice.equals("3")) System.exit(0);
+        System.out.println("Enter the size of the hash table: ");
+        tableSize = scanner.nextInt();
+        while(true)
+        {
+            printActionMenu();
+            methodChoice = scanner.next();
+            if(methodChoice.equals("6")) System.exit(0);
+            new DictionaryHandler(methodChoice, tableSize, tableChoice);
         }
     }
 }
