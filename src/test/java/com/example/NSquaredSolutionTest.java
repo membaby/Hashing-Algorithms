b2ed21c8b58967fa0ff257d0f8928f58e5c72f87
abderrahman504 <rahmankhsherb@gmail.com>
2023-05-23 23:15:00 +0300

    Finished almost all tests

--- src/test/java/com/example/NSquaredSolutionTest.java
+++ src/test/java/com/example/NSquaredSolutionTest.java
@@ -14,32 +14,19 @@ import static org.junit.Assert.assertEquals;
 public class NSquaredSolutionTest
 {
     @Test
-    public void insertionTest()
+    public void simple_insertion_test()
     {
-        NSquaredSolution hash_table = new NSquaredSolution(100);
+        NSquaredSolution hash_table = new NSquaredSolution(10);
         assertTrue(hash_table.insert("apple"));
         assertTrue(hash_table.insert("banana"));
         assertTrue(hash_table.insert("cherry"));
-        assertFalse(hash_table.insert("apple"));
-    }
-
-	@Test
-    public void insertionTest2()
-    {
-        NSquaredSolution hash_table = new NSquaredSolution(2);
-        assertTrue(hash_table.insert("l"));
-        assertTrue(hash_table.insert("6"));
-        assertTrue(hash_table.search("l"));
-        assertTrue(hash_table.search("6"));
 		
     }
 
-
-
     @Test
-    public void insertionDuplicate()
+    public void insertion_duplicate_test()
     {
-        NSquaredSolution hash_table = new NSquaredSolution(100);
+        NSquaredSolution hash_table = new NSquaredSolution(50);
         assertTrue(hash_table.insert("apple"));
         assertTrue(hash_table.insert("banana"));
         assertTrue(hash_table.insert("cherry"));
@@ -53,15 +40,325 @@ public class NSquaredSolutionTest
         assertFalse(hash_table.insert("grape"));
         assertFalse(hash_table.insert("watermelon"));
     }
+	
+    @Test
+    public void simple_search_test()
+    {
+        NSquaredSolution hash_table = new NSquaredSolution(50);
+        assertTrue(hash_table.insert("apple"));
+        assertTrue(hash_table.insert("banana"));
+        assertTrue(hash_table.insert("cherry"));
+        assertTrue(hash_table.insert("orange"));
+        assertTrue(hash_table.insert("grape"));
+        assertTrue(hash_table.insert("watermelon"));
+		
+        assertTrue(hash_table.search("apple"));
+        assertTrue(hash_table.search("banana"));
+        assertTrue(hash_table.search("cherry"));
+        assertTrue(hash_table.search("orange"));
+        assertTrue(hash_table.search("grape"));
+        assertTrue(hash_table.search("watermelon"));
+    }
+	
+    @Test
+    public void simple_search_test2()
+    {
+        NSquaredSolution hash_table = new NSquaredSolution(50);
+        assertTrue(hash_table.insert("apple"));
+        assertTrue(hash_table.insert("banana"));
+        assertTrue(hash_table.insert("cherry"));
+        assertTrue(hash_table.insert("orange"));
+        assertTrue(hash_table.insert("grape"));
+        assertTrue(hash_table.insert("watermelon"));
+		
+        assertFalse(hash_table.search("book"));
+        assertFalse(hash_table.search("car"));
+        assertFalse(hash_table.search("tree"));
+        assertFalse(hash_table.search("door"));
+        assertFalse(hash_table.search("transport"));
+        assertFalse(hash_table.search("Specific"));
+    }
 
     @Test
-    public void insertionMultiple()
+    public void delete_test()
     {
-        int countSuccessfulInsertions = 0 ;
-        NSquaredSolution hash_table = new NSquaredSolution(1000);
+        NSquaredSolution hash_table = new NSquaredSolution(50);
+        assertTrue(hash_table.insert("apple"));
+        assertTrue(hash_table.insert("banana"));
+        assertTrue(hash_table.insert("cherry"));
+        assertTrue(hash_table.insert("orange"));
+        assertTrue(hash_table.insert("grape"));
+        assertTrue(hash_table.insert("watermelon"));
+		
+        assertTrue(hash_table.delete("apple"));
+        assertTrue(hash_table.delete("banana"));
+        assertTrue(hash_table.delete("cherry"));
+        assertTrue(hash_table.delete("orange"));
+        assertTrue(hash_table.delete("grape"));
+        assertTrue(hash_table.delete("watermelon"));
+		
+    }
+	
+    @Test
+    public void search_deleted_key_test()
+    {
+        NSquaredSolution hash_table = new NSquaredSolution(50);
+        assertTrue(hash_table.insert("apple"));
+        assertTrue(hash_table.insert("banana"));
+        assertTrue(hash_table.insert("cherry"));
+        assertTrue(hash_table.insert("orange"));
+        assertTrue(hash_table.insert("grape"));
+        assertTrue(hash_table.insert("watermelon"));
+		
+        assertTrue(hash_table.delete("apple"));
+        assertTrue(hash_table.delete("banana"));
+        assertTrue(hash_table.delete("cherry"));
+        assertTrue(hash_table.delete("orange"));
+        assertTrue(hash_table.delete("grape"));
+        assertTrue(hash_table.delete("watermelon"));
+		
+        assertFalse(hash_table.search("apple"));
+        assertFalse(hash_table.search("banana"));
+        assertFalse(hash_table.search("cherry"));
+        assertFalse(hash_table.search("orange"));
+        assertFalse(hash_table.search("grape"));
+        assertFalse(hash_table.search("watermelon"));
+		
+    }
+	
+    @Test
+    public void delete_test2()
+    {
+        NSquaredSolution hash_table = new NSquaredSolution(50);
+        assertTrue(hash_table.insert("apple"));
+        assertTrue(hash_table.insert("banana"));
+        assertTrue(hash_table.insert("cherry"));
+        assertTrue(hash_table.insert("orange"));
+        assertTrue(hash_table.insert("grape"));
+        assertTrue(hash_table.insert("watermelon"));
+		
+        assertFalse(hash_table.delete("book"));
+        assertFalse(hash_table.delete("car"));
+        assertFalse(hash_table.delete("tree"));
+        assertFalse(hash_table.delete("door"));
+        assertFalse(hash_table.delete("transport"));
+        assertFalse(hash_table.delete("Specific"));
+    }
+	
+    @Test
+    public void duplicate_delete_test()
+    {
+        NSquaredSolution hash_table = new NSquaredSolution(10);
+		
+        assertTrue(hash_table.insert("apple"));
+        assertTrue(hash_table.insert("banana"));
+        assertTrue(hash_table.insert("cherry"));
+        assertTrue(hash_table.insert("orange"));
+        assertTrue(hash_table.insert("grape"));
+        assertTrue(hash_table.insert("watermelon"));
+
+		assertTrue(hash_table.delete("apple"));
+        assertTrue(hash_table.delete("banana"));
+        assertTrue(hash_table.delete("cherry"));
+        assertTrue(hash_table.delete("orange"));
+        assertTrue(hash_table.delete("grape"));
+        assertTrue(hash_table.delete("watermelon"));
+
+		assertFalse(hash_table.delete("apple"));
+        assertFalse(hash_table.delete("banana"));
+        assertFalse(hash_table.delete("cherry"));
+        assertFalse(hash_table.delete("orange"));
+        assertFalse(hash_table.delete("grape"));
+        assertFalse(hash_table.delete("watermelon"));
+    }
+	
+    @Test
+    public void insert_deleted_key_test()
+    {
+        NSquaredSolution hash_table = new NSquaredSolution(10);
+        assertTrue(hash_table.insert("apple"));
+        assertTrue(hash_table.insert("banana"));
+        assertTrue(hash_table.insert("cherry"));
+        assertTrue(hash_table.insert("orange"));
+        assertTrue(hash_table.insert("grape"));
+        assertTrue(hash_table.insert("watermelon"));
+
+		assertTrue(hash_table.delete("apple"));
+        assertTrue(hash_table.delete("banana"));
+        assertTrue(hash_table.delete("cherry"));
+        assertTrue(hash_table.delete("orange"));
+        assertTrue(hash_table.delete("grape"));
+        assertTrue(hash_table.delete("watermelon"));
+    }
+	
+    @Test
+    public void tight_insert()
+    {
+        NSquaredSolution hash_table = new NSquaredSolution(3);
+        assertTrue(hash_table.insert("orange"));
+        assertTrue(hash_table.insert("grape"));
+        assertTrue(hash_table.insert("watermelon"));
+
+        assertTrue(hash_table.search("orange"));
+        assertTrue(hash_table.search("grape"));
+        assertTrue(hash_table.search("watermelon"));
+    }
+
+    @Test
+    public void insert_10000()
+    {
+        NSquaredSolution hash_table = new NSquaredSolution(10000);
         try
         {
-            File file = new File("testFiles/1000.txt");
+            File file = new File("testFiles/10000.txt");
+            Scanner scn = new Scanner(file);
+            while(scn.hasNextLine())
+            {
+                String word = scn.nextLine();
+				assertTrue(hash_table.insert(word));
+            }
+			scn.close();
+        }
+        catch(FileNotFoundException e)
+        {
+             System.out.println("File not found");
+			 assertTrue(false);
+        }
+    }
+
+    @Test
+    public void search_10000()
+    {
+        NSquaredSolution hash_table = new NSquaredSolution(10000);
+        try
+        {
+            File file = new File("testFiles/10000.txt");
+            Scanner scn = new Scanner(file);
+            while(scn.hasNextLine())
+            {
+                String word = scn.nextLine();
+				assertTrue(hash_table.insert(word));
+            }
+			scn.close();
+			scn = new Scanner(file);
+			while(scn.hasNextLine())
+            {
+                String word = scn.nextLine();
+				assertTrue(hash_table.search(word));
+            }
+			scn.close();
+        }
+        catch(FileNotFoundException e)
+        {
+             System.out.println("File not found");
+			 assertTrue(false);
+        }
+    }
+
+    @Test
+    public void delete_10000()
+    {
+        NSquaredSolution hash_table = new NSquaredSolution(10000);
+        try
+        {
+            File file = new File("testFiles/10000.txt");
+            Scanner scn = new Scanner(file);
+            while(scn.hasNextLine())
+            {
+                String word = scn.nextLine();
+				assertTrue(hash_table.insert(word));
+            }
+			scn.close();
+			scn = new Scanner(file);
+			while(scn.hasNextLine())
+            {
+                String word = scn.nextLine();
+				assertTrue(hash_table.delete(word));
+            }
+			scn.close();
+        }
+        catch(FileNotFoundException e)
+        {
+             System.out.println("File not found");
+			 assertTrue(false);
+        }
+    }
+
+    @Test
+    public void random_search_filled()
+    {
+        NSquaredSolution mySet = new NSquaredSolution(10000);
+		HashSet<String> refSet = new HashSet<String>(10000);
+
+        try
+        {
+            File file = new File("testFiles/10000.txt");
+            Scanner scn = new Scanner(file);
+            while(scn.hasNextLine())
+            {
+                String word = scn.nextLine();
+				assertTrue(mySet.insert(word));
+				refSet.add(word);
+            }
+			scn.close();
+			File searchF = new File("comparisonFiles/200000.txt");
+			scn = new Scanner(searchF);
+			while(scn.hasNextLine())
+            {
+                String word = scn.nextLine();
+				assertEquals(mySet.search(word), refSet.contains(word));
+            }
+			scn.close();
+        }
+        catch(FileNotFoundException e)
+        {
+             System.out.println("File not found");
+			 assertTrue(false);
+        }
+    }
+
+    @Test
+    public void random_delete_filled()
+    {
+        NSquaredSolution mySet = new NSquaredSolution(10000);
+		HashSet<String> refSet = new HashSet<String>(10000);
+
+        try
+        {
+            File file = new File("testFiles/10000.txt");
+            Scanner scn = new Scanner(file);
+            while(scn.hasNextLine())
+            {
+                String word = scn.nextLine();
+				assertTrue(mySet.insert(word));
+				refSet.add(word);
+            }
+			scn.close();
+			File searchF = new File("comparisonFiles/200000.txt");
+			scn = new Scanner(searchF);
+			while(scn.hasNextLine())
+            {
+                String word = scn.nextLine();
+				assertEquals(mySet.delete(word), refSet.remove(word));
+				assertFalse(mySet.search(word));
+            }
+			scn.close();
+        }
+        catch(FileNotFoundException e)
+        {
+             System.out.println("File not found");
+			 assertTrue(false);
+        }
+    }
+
+    @Test
+    public void insert_file_with_duplicates()
+    {
+        int countSuccessfulInsertions = 0 ;
+        NSquaredSolution hash_table = new NSquaredSolution(5000);
+        try
+        {
+            File file = new File("testFiles/5000.txt");
             Scanner scanFile = new Scanner(file);
             while(scanFile.hasNextLine())
             {
@@ -72,6 +369,7 @@ public class NSquaredSolutionTest
                     assertTrue(hash_table.insert(word));
                     countSuccessfulInsertions++;
                 }
+
             }
         }
         catch(FileNotFoundException e)
@@ -79,8 +377,10 @@ public class NSquaredSolutionTest
             System.out.println("File not found");
             e.printStackTrace();
         }
-        assertEquals(999, countSuccessfulInsertions);
+
+        assertEquals(2122, countSuccessfulInsertions);
     }
+
 
     @Test
     public void insertionHuge()
@@ -112,63 +412,6 @@ public class NSquaredSolutionTest
     }
 
 	@Test
-	public void insert500()
-	{
-		HashSet<String> refSet = new HashSet<String>(500);
-		NSquaredSolution mySet = new NSquaredSolution(500);
-		try{
-			File file = new File("comparisonFiles/500.txt");
-            Scanner scn = new Scanner(file);
-			while(scn.hasNext())
-			{
-				String next = scn.next();
-				assertEquals(refSet.add(next), mySet.insert(next));
-			}
-			for (String string : refSet) {
-				assertTrue(mySet.search(string));
-			}
-		} catch (FileNotFoundException e){assertTrue(false);}
-	}
-
-	@Test
-	public void insert1000()
-	{
-		HashSet<String> refSet = new HashSet<String>(1000);
-		NSquaredSolution mySet = new NSquaredSolution(1000);
-		try{
-			File file = new File("comparisonFiles/1000.txt");
-            Scanner scn = new Scanner(file);
-			while(scn.hasNext())
-			{
-				String next = scn.next();
-				assertEquals(refSet.add(next), mySet.insert(next));
-			}
-			for (String string : refSet) {
-				assertTrue(mySet.search(string));
-			}
-		} catch (FileNotFoundException e){assertTrue(false);}
-	}
-
-	@Test
-	public void insert5000()
-	{
-		HashSet<String> refSet = new HashSet<String>(5000);
-		NSquaredSolution mySet = new NSquaredSolution(5000);
-		try{
-			File file = new File("comparisonFiles/5000.txt");
-            Scanner scn = new Scanner(file);
-			while(scn.hasNext())
-			{
-				String next = scn.next();
-				assertEquals(refSet.add(next), mySet.insert(next));
-			}
-			for (String string : refSet) {
-				assertTrue(mySet.search(string));
-			}
-		} catch (FileNotFoundException e){assertTrue(false);}
-	}
-
-	@Test
 	public void insert10000()
 	{
 		HashSet<String> refSet = new HashSet<String>(10000);
@@ -186,116 +429,4 @@ public class NSquaredSolutionTest
 			}
 		} catch (FileNotFoundException e){assertTrue(false);}
 	}
-
-
-    @Test
-    public void deletionTest(){
-        NSquaredSolution hash_table = new NSquaredSolution(100);
-        hash_table.insert("apple");
-        hash_table.insert("banana");
-        hash_table.insert("cherry");
-        assertTrue(hash_table.delete("apple"));
-        assertFalse(hash_table.delete("apple"));
-    }
-
-    @Test
-    public void SearchTest(){
-        NSquaredSolution hash_table = new NSquaredSolution(100);
-        hash_table.insert("apple");
-        hash_table.insert("banana");
-        hash_table.insert("cherry");
-        assertTrue(hash_table.search("apple"));
-        assertTrue(hash_table.search("banana"));
-        assertTrue(hash_table.search("cherry"));
-        assertFalse(hash_table.search("orange"));
-    }
-
-    @Test
-    public void hugeSearch1()
-    {
-        NSquaredSolution hash_table = new NSquaredSolution(10);
-        for(int i = 0; i < 1000000; i++)
-        {
-            hash_table.search(String.valueOf((i)));
-        }
-    }
-
-    @Test
-    public void hugeSearch2()
-    {
-        NSquaredSolution hash_table = new NSquaredSolution(10);
-        for(int i = 0; i < 500000; i++)
-        {
-            hash_table.search(String.valueOf((i)));
-        }
-    }
-    
-    @Test
-    public void hugeSearch3()
-    {
-        NSquaredSolution hash_table = new NSquaredSolution(10);
-
-        for(int i = 0; i < 100000; i++)
-        {
-            hash_table.search(String.valueOf((i)));
-        }
-    }
-
-
-
-    @Test
-    public void searchAndDelete(){
-        NSquaredSolution hash_table = new NSquaredSolution(100);
-        hash_table.insert("apple");
-        hash_table.insert("banana");
-        hash_table.insert("cherry");
-        assertTrue(hash_table.search("apple"));
-        assertTrue(hash_table.delete("apple"));
-        assertFalse(hash_table.search("apple"));
-    }
-
-    @Test
-    public void hugeSearchAndDelete(){
-
-        NSquaredSolution hash_table = new NSquaredSolution(10000);
-        //setup the table with 10 thousand words
-        for (int i = 0; i < 10000; i++)
-        {
-            assertTrue(hash_table.insert(String.valueOf((i))));
-        }
-        //search for the words
-        for (int i = 0; i < 10000; i++)
-        {
-            assertTrue(hash_table.search(String.valueOf((i))));
-        }
-        //delete the words
-        for (int i = 0; i < 10000; i++)
-        {
-            assertTrue(hash_table.delete(String.valueOf((i))));
-        }
-    }
-
-    @Test
-    //number of times needed to rebuild the hash table in case of collision
-    public void rebuildTest(){
-        NSquaredSolution hash_table = new NSquaredSolution(100);
-        hash_table.insert("apple");
-        hash_table.insert("banana");
-        hash_table.insert("cherry");
-        hash_table.insert("orange");
-        hash_table.insert("grape");
-//        assertThat(hash_table.getRebuildCount(), is(1));
-    }
-
-    @Test
-    //test space of the hash table
-    public void spaceTest(){
-        NSquaredSolution hash_table = new NSquaredSolution(100);
-        hash_table.insert("apple");
-        hash_table.insert("banana");
-        hash_table.insert("cherry");
-        hash_table.insert("orange");
-        hash_table.insert("grape");
-//        assertThat(hash_table.getSpace(), is(5));
-    }
 }
